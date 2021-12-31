f5c9e11dd2e77b98d59afe59e4f2530df57b2f8a
josephrockqz <josephrockqz@gmail.com>
2021-12-31 12:54:59 -0600

    added speed miner to games nav

--- src/views/Games.vue
+++ src/views/Games.vue
@@ -1,10 +1,30 @@
 <template>
-    <div class="page-view">
-      <span class="page-view-span">
+
+    <div>
+
+      <!-- <div class="game-div">
+        <h4>Click to play Speed Miner</h4>
+        <a href="https://speedminer.joerock.dev" target="_blank"><img src="../../public/mine.png"/></a>
+      </div>
+
+      <div class="game-div">
         <h4>Click to play Blue 2 Red</h4>
         <a href="https://blue2red.joerock.dev" target="_blank"><img src="../../public/blue2red.png"/></a>
-      </span>
-    </div>   
+      </div> -->
+
+      <div id='wrapper' style='text-align: center;'>
+        <div class="game-div" style="display: inline-block; vertical-align: top;">
+          <h4>Click Image Below to play Speed Miner</h4>
+          <a href="https://speedminer.joerock.dev" target="_blank"><img src="../../public/mine.png"/></a>
+        </div>
+        <div class="game-div" style="display: inline-block; vertical-align: top;">
+          <h4>Click Image Below to play Blue 2 Red</h4>
+          <a href="https://blue2red.joerock.dev" target="_blank"><img src="../../public/blue2red.png"/></a>
+        </div>
+      </div>
+
+    </div>
+    
 </template>
 
 <script>
@@ -14,5 +34,8 @@ export default {
 </script>
 
 <style scoped>
-
+.game-div {
+  
+  margin: 20px;
+}
 </style>
